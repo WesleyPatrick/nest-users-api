b0e7b48bdff566c7a38b18f9b7fd743757981970
Wesley Patrick <wesleypatrick0909@gmail.com>
2026-01-31 20:16:26 -0300

    Add SearchResult class and SearchResultProps type to enhance search functionality, providing structured results with pagination and sorting options.

--- src/shared/domain/repositories/searchable-repository-contract.ts
+++ src/shared/domain/repositories/searchable-repository-contract.ts
@@ -11,6 +11,17 @@ export type SearchProps<Filter = string> = {
   filter?: Filter | null;
 }
 
+export type SearchResultProps<E extends Entity, Filter> = {
+  items: E[];
+  total: number;
+  currentPage: number;
+  perPage: number;
+  lastPage: number;
+  sort: string | null;
+  sortDir: SortDirection | null;
+  filter: Filter | null;
+}
+
 export class SearchParams {
   protected _page: number;
   protected _perPage: number;
@@ -19,11 +30,11 @@ export class SearchParams {
   protected _filter: string | null;
 
   constructor(props: SearchProps) {
-    this._page = props.page ?? 1;
-    this._perPage = props.perPage ?? 15;
-    this._sort = props.sort ?? null;
-    this._sortDir = props.sortDir ?? null;
-    this._filter = props.filter ?? null;
+    this.page = props.page ?? 1;
+    this.perPage = props.perPage ?? 15;
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
   }
 
   get page(): number {
@@ -86,6 +97,40 @@ export class SearchParams {
   }
 }
 
+export class SearchResult<E extends Entity, Filter> {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly lastPage: number;
+  readonly sort: string | null;
+  readonly sortDir: SortDirection | null;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.currentPage = props.currentPage;
+    this.perPage = props.perPage;
+    this.lastPage = Math.ceil(this.total / this.perPage);
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
+  }
+
+  toJSON(forceEntity = false): SearchResultProps<E, Filter> {
+    return {
+      items: this.items.map(item => forceEntity ? item : item.toJSON()),
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter,
+    }
+  }
+}
 
 export interface SearchableRepositoryContract<
 E extends Entity,
