a1aef3ea921678d1ad93b6be454eabf9a2a9e5e1
Wesley Patrick <wesleypatrick0909@gmail.com>
2026-01-31 14:13:21 -0300

    Adding users resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { EnvConfigModule } from './shared/infraestructure/env-config/env-config.module';
-
+import { UsersModule } from './users/infra/dto/users.module';
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
